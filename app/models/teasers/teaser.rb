ed754bfa7bf07b5efa2e7c593cdf6ab91e442f7a
PeoneEr <peoneerko@yandex.ru>
2015-02-27 15:22:41 +0600

    ria tomsk teaser

--- app/models/teasers/teaser.rb
+++ app/models/teasers/teaser.rb
@@ -1,29 +1,17 @@
 class Teaser < ActiveRecord::Base
   extend FriendlyId
 
-  attr_accessor :related_items, :need_change
-
   attr_accessible :items_quantity, :title
 
   validates_presence_of :title
   validates_presence_of :items_quantity
   has_many :teaser_items, :dependent => :destroy
 
-  after_create :create_teaser_items
-
   friendly_id :title, :use => :slugged
   def should_generate_new_friendly_id?
     return true if !self.slug?
 
     false
-  end
-
-  private
-
-  def create_teaser_items
-    (1..self.items_quantity).each do
-      teaser_items.create
-    end
   end
 end
 
@@ -38,5 +26,6 @@ end
 #  slug           :string(255)
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
+#  with_relations :boolean
 #
 
