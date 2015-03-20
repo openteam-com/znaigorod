ca05379af8bad98a960f73df4d9080498c19ef4a
Karmickoala904 <karmickoala904@gmail.com>
2015-03-20 15:23:36 +0600

    create_organization

--- db/schema.rb
+++ db/schema.rb
@@ -1373,7 +1373,6 @@ ActiveRecord::Schema.define(:version => 20150416034900) do
     t.string   "slug"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-    t.boolean  "with_relations"
   end
 
   create_table "tickets", :force => true do |t|
