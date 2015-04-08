4169d2adce45408c27b81ccdfaaf79b093f0b48b
Karmickoala904 <karmickoala904@gmail.com>
2015-04-08 15:29:06 +0600

    review on /:slug

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -12,6 +12,7 @@ class OrganizationsController < ApplicationController
       format.html {
         @presenter = OrganizationsPresenterBuilder.new(params).build
         @categories = @presenter.category ? @presenter.category.root.children : OrganizationCategory.used_roots
+        @reviews = ReviewDecorator.decorate(OrganizationCategory.find(params[:slug]).reviews) if params[:slug]
 
         if request.xhr?
           if @presenter.view_type == 'list'
