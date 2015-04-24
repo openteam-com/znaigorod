f4da6513ed72ae81cf6f7566438fb940202bf950
PeoneEr <peoneerko@yandex.ru>
2015-04-24 15:10:31 +0600

    start discounts archive

--- lib/draft_published_states.rb
+++ lib/draft_published_states.rb
@@ -2,10 +2,11 @@ module DraftPublishedStates
   extend ActiveSupport::Concern
 
   included do
-    scope :by_kind,   ->(kind) { where(:kind => kind) }
-    scope :by_state,  ->(state) { where(:state => state) }
+    scope :by_kind,   -> (kind) { where(:kind => kind) }
+    scope :by_state,  -> (state) { where(:state => state) }
     scope :draft,     -> { with_state(:draft) }
     scope :published, -> { with_state(:published) }
+    scope :archive,   -> { with_state(:archive) }
 
     state_machine :initial => :draft do
       after_transition any => :published do |object, transition|
@@ -18,6 +19,10 @@ module DraftPublishedStates
 
       event :to_published do
         transition :draft => :published
+      end
+
+      event :to_archive do
+        transition :published => :archive
       end
 
       event :to_draft do
