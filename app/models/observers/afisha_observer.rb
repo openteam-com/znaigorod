8bf0cc4de9117dab7cb2a0650a9ced2ed9429f2d
Karmickoala904 <karmickoala904@gmail.com>
2015-07-14 12:48:12 +0600

    remove delay on reindex showings

--- app/models/observers/afisha_observer.rb
+++ app/models/observers/afisha_observer.rb
@@ -48,7 +48,8 @@ class AfishaObserver < ActiveRecord::Observer
 
     return unless afisha.published?
 
-    afisha.delay(:queue => 'critical').reindex_showings
+    #afisha.delay(:queue => 'critical').reindex_showings
+    afisha.reindex_showings
 
     afisha.delay(:queue => 'critical').upload_poster_to_vk if (afisha.poster_vk_id.nil? || afisha.poster_url_changed?) && afisha.poster_url?
   end
