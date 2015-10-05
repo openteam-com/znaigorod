554c1526f9aee8b00e682e1cb926af063f7e560b
Karmickoala904 <karmickoala904@gmail.com>
2015-10-05 16:51:06 +0600

    typo fix

--- app/controllers/sled_controller.rb
+++ app/controllers/sled_controller.rb
@@ -1,6 +1,6 @@
 class SledController < ApplicationController
   def index
-    not_in_contest = Account.find([11])
+    not_in_contest = Account.find([37976])
     @accounts = Account.joins(:users).joins('LEFT JOIN roles ON users.id = roles.user_id').where('roles.user_id is null')
       .joins(:reviews).where('reviews.created_at > ?', Time.zone.parse("01.10.2015").beginning_of_day).where('reviews.state = ?', "published").uniq
       .sort_by{ |account| account.sled_rating }.reverse - not_in_contest
