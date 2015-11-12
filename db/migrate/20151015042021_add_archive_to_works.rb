3226df3f95e325cdaacac9d439bae4cda6ddd80f
PeoneEr <peoneerko@yandex.ru>
2015-11-12 10:01:55 +0600

    changed phoned for sevastopol

--- db/migrate/20151015042021_add_archive_to_works.rb
+++ db/migrate/20151015042021_add_archive_to_works.rb
@@ -1,6 +1,10 @@
 class AddArchiveToWorks < ActiveRecord::Migration
   def change
     add_column :works, :archive, :boolean, :default => false
-    Contest.find("miss-pivovarochka-2015-v-tomske").works.update_all(:archive => true)
+
+    begin
+      Contest.find("miss-pivovarochka-2015-v-tomske").works.update_all(:archive => true)
+    rescue
+    end
   end
 end
