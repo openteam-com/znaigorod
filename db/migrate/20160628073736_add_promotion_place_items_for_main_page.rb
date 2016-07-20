14c22015853b049925163006268a459e8093342c
sergocap <systemofadown.2013@yandex.ru>
2016-07-20 16:00:15 +0700

    fix migrate with promotion

--- db/migrate/20160628073736_add_promotion_place_items_for_main_page.rb
+++ db/migrate/20160628073736_add_promotion_place_items_for_main_page.rb
@@ -1,14 +1,15 @@
 class AddPromotionPlaceItemsForMainPage < ActiveRecord::Migration
   def up
-    Promotion.find(15)
+    main_page_promo_id = Setting['app.city'] == 'sevastopol' ? 13 : 15
+    Promotion.find(main_page_promo_id)
       .promotion_places.second
       .place_items
       .create(:url => '/main_page_afisha?content=afisha', :starts_at => DateTime.now, :ends_at => DateTime.now + 30.year, :title => 'Афиша мероприятий Томска')
-    Promotion.find(15)
+    Promotion.find(main_page_promo_id)
       .promotion_places[2]
       .place_items
       .create(:url => '/main_page_photogalleries?content=photogalleries', :starts_at => DateTime.now, :ends_at => DateTime.now + 30.year, :title => 'Фотостримы Томска')
-    Promotion.find(15)
+    Promotion.find(main_page_promo_id)
       .promotion_places[3]
       .place_items
       .create(:url => '/main_page_discounts?content=discounts', :starts_at => DateTime.now, :ends_at => DateTime.now + 30.year, :title => 'Скидки, сертификаты и купоны, акции и распродажи в Томске. Индивидуальный подбор.')
