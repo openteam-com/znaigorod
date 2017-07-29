d5a17baef039ce27242be2a5baa7cc8096c94d24
sergocap <systemofadown.2013@yandex.ru>
2017-07-29 16:27:08 +0700

    affiche_schedule export

--- app/api/export/api.rb
+++ app/api/export/api.rb
@@ -11,20 +11,29 @@ module Export
         requires :count, type: Integer
       end
       get '/' do
-        Afisha.published.actual.with_showings.limit(params[:count]).offset(params[:offset]).map {|afisha|
+        Afisha.with_showings.published
+          .limit(params[:count]).offset(params[:offset]).map {|afisha|
           {
             remote_id: afisha.id,
             title: afisha.title,
             image_url: afisha.image_url,
             description: afisha.description,
-            type: afisha.kind.to_a.first,
+            type: afisha.kind.to_a,
             original_title: afisha.original_title,
             trailer_code: afisha.trailer_code,
             tags: afisha.tag,
+            constant: afisha.constant,
             age_min: afisha.age_min,
             vk_event_url: afisha.vk_event_url,
-            schedules: afisha.showings.actual.where('starts_at < ? AND ends_at < ?', Date.current + 6.months, Date.current + 7.months)
-            .select('latitude, longitude, place, price_min, price_max, starts_at, ends_at'),
+            showings: afisha.constant ? [] :
+              afisha.showings.actual.where('starts_at < ?', Date.current + 2.months)
+                .select('latitude, longitude, place, price_min, price_max, starts_at, ends_at'),
+            schedule: !(afisha.constant && afisha.affiche_schedule.present?) ? nil :
+              [
+                :starts_on, :ends_on, :starts_at, :ends_at,
+                :holidays, :place, :price_max, :price_min,
+                :longitude, :latitude
+              ].inject({}) {|hash, field| hash[field] = afisha.affiche_schedule.send(field); hash },
             images: afisha.gallery_images.pluck(:file_url)
           }
         }
